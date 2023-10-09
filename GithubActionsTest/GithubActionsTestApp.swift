0efd438d90305ba73e60e43b0593267c57f84305
Abdelrhman Elmahdy <Abdelrahman.elmahdy@btech.com>
2023-10-09 16:02:36 +0300

    first commit

---
+++ GithubActionsTest/GithubActionsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionsTestApp.swift
+//  GithubActionsTest
+//
+//  Created by Abdelrhman Elmahdy on 09/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct GithubActionsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
